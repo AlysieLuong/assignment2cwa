bb86e8c9901f4d32280d8694a1cbe2a202439d7f
AlysieLuong <alysieluongg@gmail.com>
2025-10-21 01:56:06 +1100

    adding video 2

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -15,7 +15,6 @@ export default function About() {
         <p>My student number is 21612368</p>
         <p>This is a video on explaining what features my website has and the codes used</p>
         <h2 style = {{ marginTop: "1rem"}}><b>Video 1 - Assignment 1</b></h2>
-        
         {/* assignment 1 video*/}
         <video controls style={{ width: "100%", maxWidth: "600px" }}>
           <source src="/cse3cwassignment1video.mp4" type="video/mp4" />
@@ -23,6 +22,10 @@ export default function About() {
 
         <h2><b>Video 2 - Assignment 2</b></h2>
         {/* assignment 2 video*/}
+        <video controls style={{ width: "100%", maxWidth: "600px" }}>
+          <source src="/cse3cwassignment2video.mp4" type="video/mp4" />
+        </video>
+
 
         <h2><b>Feedback - Assignment 2</b></h2>
         <div style={{ display: "flex", flexDirection: "column",gap: "1rem", flexWrap: "wrap" }}>
